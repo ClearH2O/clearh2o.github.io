bc546ffcde4b7d778e0b29bfdc12f2122d4e0777
mJfJ <moh.jamato@programcouncil.com>
2015-12-11 11:23:05 -0700

    fixed stuffs

--- js/grayscale.js
+++ js/grayscale.js
@@ -41,7 +41,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(39.993021, -105.090509), // Lafayette CO
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
